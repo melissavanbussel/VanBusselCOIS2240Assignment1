7abe2acfa4b228951e20b03218d50a101cb04812
unknown <melissavanbussel@trentu.ca>
2019-01-29 17:22:18 -0500

    Added fibSeriesIte method and used it in main method

--- question2_VanBussel_Melissa_Assignment1/src/question2_VanBussel_Melissa_Assignment1.java
+++ question2_VanBussel_Melissa_Assignment1/src/question2_VanBussel_Melissa_Assignment1.java
@@ -9,11 +9,24 @@ public class question2_VanBussel_Melissa_Assignment1 {
         for (int i = 0; i < nFibonacci; i++) {
             System.out.print(fibSeriesRec(i) + " ");
         }
+        System.out.println("The first " + nFibonacci + " integers in the Fibonacci Sequence, using the iterative method, are: ");
+        for (int i = 0; i < nFibonacci; i++) {
+            System.out.print(fibSeriesIte(i) + " ");
+        }
     }
     public static int fibSeriesRec(int n) {
         if ((n == 0) || (n == 1))
             return n;
         else
             return fibSeriesRec(n - 1) + fibSeriesRec(n - 2);
+    }
+    public static int fibSeriesIte(int n) {
+        int firstFib = 0, secondFib = 1, newFib = 1;
+        for (int i = 0; i < n; i++) {
+            firstFib = secondFib;
+            secondFib = newFib;
+            newFib = firstFib + secondFib;
+        }
+        return firstFib;
     }
 }
