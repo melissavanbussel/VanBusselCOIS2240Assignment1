bff9e12f064805d43b052e60547a332201a578bb
unknown <melissavanbussel@trentu.ca>
2019-01-29 17:50:24 -0500

    Used System.nanoTime() to check if fibSeriesRec or fibSeriesIte is faster

--- question2_VanBussel_Melissa_Assignment1/src/question2_VanBussel_Melissa_Assignment1.java
+++ question2_VanBussel_Melissa_Assignment1/src/question2_VanBussel_Melissa_Assignment1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 public class question2_VanBussel_Melissa_Assignment1 {
     public static void main(String[] args) {
         int nFibonacci;
@@ -6,12 +7,28 @@ public class question2_VanBussel_Melissa_Assignment1 {
         System.out.print("Enter the value of n >> ");
         nFibonacci = userInput.nextInt();
         System.out.println("The first " + nFibonacci + " integers in the Fibonacci Sequence, using the recursive method, are: ");
+        long startTimeRec = System.nanoTime();
         for (int i = 0; i < nFibonacci; i++) {
             System.out.print(fibSeriesRec(i) + " ");
         }
+        long endTimeRec = System.nanoTime();
+        long elapsedTimeRec = endTimeRec - startTimeRec;
+        System.out.println("\nThe elapsed time for the recursive method is " + elapsedTimeRec + " nanoseconds");
         System.out.println("The first " + nFibonacci + " integers in the Fibonacci Sequence, using the iterative method, are: ");
+        long startTimeIte = System.nanoTime();
         for (int i = 0; i < nFibonacci; i++) {
             System.out.print(fibSeriesIte(i) + " ");
+        }
+        long endTimeIte = System.nanoTime();
+        long elapsedTimeIte = endTimeIte - startTimeIte;
+        System.out.println("\nThe elapsed time for the recursive method is " + elapsedTimeIte + " nanoseconds");
+        // Check which method was faster and print out by how much
+        if (elapsedTimeRec > elapsedTimeIte) {
+            long amountFaster = elapsedTimeRec - elapsedTimeIte;
+            System.out.println("The recursive method was faster by " + amountFaster + " nanoseconds");
+        } else {
+            long amountFaster = elapsedTimeIte - elapsedTimeRec;
+            System.out.print("The iterative method was faster by " + amountFaster + " nanoseconds");
         }
     }
     public static int fibSeriesRec(int n) {
